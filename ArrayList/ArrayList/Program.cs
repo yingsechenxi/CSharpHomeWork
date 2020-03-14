ed47e2e71d810369f710fe2ba88c4a277ecf4b1a
1378212186@qq.com <1378212186@qq.com>
2020-03-14 11:24:51 +0800

    第四次作业

---
+++ ArrayList/ArrayList/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ArrayList
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public GenericList()
+        {
+            head = tail = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> a = head; a != null; a = a.Next)
+            {
+                action(a.Data);
+            }
+        }
+    }
+class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0, min = 0, max = 0;
+            GenericList<int> firstlist = new GenericList<int>();
+            firstlist.ForEach(x => Console.WriteLine(x));
+            firstlist.ForEach(x => sum += x);
+            firstlist.ForEach(x => { if (max < x) max = x; });
+            firstlist.ForEach(x => { if (min > x) min = x; });
+            Console.Write(sum+" "+max+" "+min);
+        }
+    }
+}
---
+++ Timer/Timer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Timer
+{
+    public delegate void TickHandler(int time);
+    public delegate void AlarmHandler(int time);
+    public class Clock
+    {
+        public event TickHandler OnTick;
+        public event AlarmHandler OnAlarm;
+        public void Tick(int time)
+        {
+            OnTick(time);
+        }
+        public void Alarm(int time)
+        {
+            OnAlarm(time);
+        }
+    }
+    public class TickandAlarm
+    {
+        public Clock clock1 = new Clock();
+        public int tickTock;
+        public TickandAlarm()
+        {
+            tickTock = 0;
+            clock1.OnTick += new TickHandler(Clock_OnTick);
+            clock1.OnAlarm += new AlarmHandler(Clock_OnAlarm);
+            void Clock_OnTick(int tickTock)
+            {
+                Console.WriteLine("Clock ticks at " + tickTock);
+            }
+            void Clock_OnAlarm(int tickTock)
+            {
+                Console.WriteLine("Clock alarms at " + tickTock);
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TickandAlarm testClock = new TickandAlarm();
+            for(int i=1;i<=10;i++)
+            {
+                testClock.tickTock = i;
+                //testClock.clock1.Tick(i);
+                //testClock.clock1.Alarm(i);
+            }
+        }
+    }
+}
